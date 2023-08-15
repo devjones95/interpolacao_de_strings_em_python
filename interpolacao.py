443905b58d451221f346bdbd82f3f30bc3009006
devjones95 <jaumassis@gmail.com>
2023-08-15 20:15:12 -0300

    Commit de envio

---
+++ interpolacao.py
@@ -0,0 +1,28 @@
+#INTERPOLAÇÂO DE STRINGS
+# Estilo antigo de interpolação (Método não mais utilizado)
+nome = "John"
+idade = 28
+profissao = "Data Analytics"
+linguagem = "Python"
+
+print("Oi, me chamo %s. Eu tenho %d anos de idade, trabalho como %s, e estou matriculado no curso de %s."% (nome, idade, profissao, linguagem))
+
+#Legendas: 
+# %s = tipo string
+# %d = tipo inteiras
+
+
+
+
+# Estilo Format (Melhor que a anterior, porém apresenta o mesmo problema)
+nome = "John"
+idade = 27 
+profissao = "Data Analytics"
+linguagem = "Python"
+
+print("Oi, me chamo {1}, e tenho {2}anos de idade. Trabalho como {3} e estou aprendendo a linguagem {4}.".format(nome, idade, profissao, linguagem))
+
+# Método de variáveis nomeadas
+
+print("Oi, me chamo {nome}, tenho {idade} anos de idade, trabalho como {profissao}, e estudo a linguagem {}")
+
